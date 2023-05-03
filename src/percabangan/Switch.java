1ad20bd2307d40862107035048c684e10ca48b8a
anyariz02 <anyanime.id@gmail.com>
2023-05-03 13:24:45 +0800

    Percabangan switch dengan variabel char

--- src/percabangan/Switch.java
+++ src/percabangan/Switch.java
@@ -21,5 +21,28 @@ public class Switch {
                 break;
         }
 
+        char nilai='D';
+        switch (nilai) {
+            case 'A':
+                System.out.println("Mhs   : \"Terima Kasih pak\"");
+                System.out.println("Dosen : \"Selamat Ya !!\"");
+                break;
+            case 'B':
+                System.out.println("Mhs   : \"Kenapa ulun kada dapat A, pak?\"");
+                System.out.println("Dosen : \"3&65#?\"");
+                break;
+            case 'C':
+                System.out.println("Mhs   : \"Ulun turun pul Pak ai, tugas pul jua\"");
+                System.out.println("Dosen : \"Tapi bisa lah menjawab ujian ??\"");
+                System.out.println("Mhs   : \"Hihihi...\"");
+                break;
+            default:
+                System.out.println("Mhs   : \"Ulun turun pul Pak ai, tugas pul jua\"");
+                System.out.println("Dosen : \"Bujur jua kah?\"");
+                System.out.println("Mhs   : \"*memeriksa berkas\"");
+                System.out.println("Dosen : \"*menceleng\"");
+                System.out.println("Dosen : \"Kabur...\"");
+                break;
+        }
     }
 }
