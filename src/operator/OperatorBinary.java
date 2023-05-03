6e15a493128b9da3d49afbc23248bda4ffa3543c
anyariz02 <anyanime.id@gmail.com>
2023-05-03 12:39:11 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,14 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assignment/penugasan
+    //menempatkan nilai kedalamm variable
+    //bari 5-7 adalah comment/koment
+
+    String nama = "Rizwan Fauzie Maulana";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+
+
+}
