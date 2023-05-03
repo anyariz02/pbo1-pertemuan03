ca23b196abc491cf74d2cbb2d89546e302f9e9ad
anyariz02 <anyanime.id@gmail.com>
2023-05-03 13:08:52 +0800

    If Else jika kondisi bernilai true, maka ada kembalian uang

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -2,13 +2,18 @@ package percabangan;
 
 public class IfElse {
     public static void main(String[] args) {
-        double totalBelanja = 25000;
+        double totalBelanja = 40000;
         double uangDiDompet = 50000;
 
         if(uangDiDompet<totalBelanja){
             System.out.println("Uang kurang, kurangi jajan anda");
-        }else{
-            System.out.println("Uang cukup, selamat menikmati");
+        }
+        else if(uangDiDompet>totalBelanja){
+            double angsul = uangDiDompet-totalBelanja;
+            System.out.println("Uang cukup, angsulannya "+angsul);
+        }
+        else {
+            System.out.println("Uang pas.. UwU..");
         }
     }
 }
